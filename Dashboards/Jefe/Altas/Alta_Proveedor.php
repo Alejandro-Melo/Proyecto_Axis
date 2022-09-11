81310b30482c61487fee91ff1854db055f7a7c9c
Alejandro <alejandromelomaggio@gmail.com>
2022-09-11 09:28:28 -0300

    Rutas de css cambiadas y arregladas.

--- Dashboards/Jefe/Altas/Alta_Proveedor.php
+++ Dashboards/Jefe/Altas/Alta_Proveedor.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../../css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../../../css/bootstrap.min.css" />
     <link
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"
     />
-    <link rel="stylesheet" href="../../css/dataTables.bootstrap5.min.css" />
-    <link rel="stylesheet" href="../../css/style.css" />
-    <link rel="stylesheet" href="../../css/styleGeneral.css">
+    <link rel="stylesheet" href="../../../css/dataTables.bootstrap5.min.css" />
+    <link rel="stylesheet" href="../../../css/style.css" />
+    <link rel="stylesheet" href="../../../css/styleGeneral.css">
     <title>Dashboard Jefe</title>
   </head>
   <body>
@@ -307,11 +307,11 @@
       </div>
     </main>
 
-    <script src="../../js/bootstrap.bundle.min.js"></script>
+    <script src="../../../js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.0.2/dist/chart.min.js"></script>
-    <script src="../../js/jquery-3.5.1.js"></script>
-    <script src="../../js/jquery.dataTables.min.js"></script>
-    <script src="../../js/dataTables.bootstrap5.min.js"></script>
-    <script src="../../js/script.js"></script>
+    <script src="../../../js/jquery-3.5.1.js"></script>
+    <script src="../../../js/jquery.dataTables.min.js"></script>
+    <script src="../../../js/dataTables.bootstrap5.min.js"></script>
+    <script src="../../../js/script.js"></script>
   </body>
 </html>
