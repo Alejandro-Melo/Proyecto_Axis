ffcb0159de7e0978e49e92838106403e1cf7dbe2
Alejandro <alejandromelomaggio@gmail.com>
2022-09-27 23:41:03 -0300

    Corroboración de correo electrónico ya existente en base de datos previo a registro de nuevo usuario.

--- Dashboards/Jefe/SQL/Alta_Usuario.php
+++ Dashboards/Jefe/SQL/Alta_Usuario.php
@@ -2,10 +2,22 @@
 
 function Alta_SQL($tipo_usuario, $contra, $email, $conexion){
 
-$sql = "INSERT INTO `usuario` (`ID_U`, `Tipo_usuario`, `Contrasenia`, `Date_creation`, `Email`, `Activo`) 
-VALUES (NULL, '$tipo_usuario', '$contra', current_timestamp(), '$email', '1');";
+$sql = "SELECT * FROM Usuario WHERE Email = '$email'";
 
-mysqli_query($conexion, $sql);
+$result = mysqli_query($conexion, $sql);
+
+$row = mysqli_num_rows($result);
+
+if($row == 0){
+
+    $sql = "INSERT INTO `usuario` (`ID_U`, `Tipo_usuario`, `Contrasenia`, `Date_creation`, `Email`, `Activo`) 
+    VALUES (NULL, '$tipo_usuario', '$contra', current_timestamp(), '$email', '1');";
+
+    mysqli_query($conexion, $sql);
+    
+} else{
+    echo "<h1>El usuario ya existe.</h1>";
+}
 
 $conexion->close();
 
--- SQL_Login_Registro/Alta_Cliente.php
+++ SQL_Login_Registro/Alta_Cliente.php
@@ -2,10 +2,22 @@
 
 function Alta_Cliente($contra, $email, $conexion){
 
-$sql = "INSERT INTO `usuario` (`ID_U`, `Tipo_usuario`, `Contrasenia`, `Date_creation`, `Email`, `Activo`) 
+$sql = "SELECT * FROM Usuario WHERE Email = '$email'";
+
+$result = mysqli_query($conexion, $sql);
+
+$row = mysqli_num_rows($result);
+
+if($row == 0){
+
+$sql2 = "INSERT INTO `Usuario` (`ID_U`, `Tipo_usuario`, `Contrasenia`, `Date_creation`, `Email`, `Activo`) 
 VALUES (NULL, 'Cliente', '$contra', current_timestamp(), '$email', 'false');";
 
-mysqli_query($conexion, $sql);
+mysqli_query($conexion, $sql2);
+
+} else{
+    echo "<h1>Su correo ya existe.</h1>";
+}
 
 $conexion->close();
 
